07350d10fd3ca910c1ccc40ed11fbfa06976aa0e
Daniel Malaquias <daniel.malaquias.silva@gmail.com>
2021-11-25 13:24:31 -0300

    fix(scripts/copy-to): multiline command

--- src/copy-to.sh
+++ src/copy-to.sh
@@ -3,9 +3,9 @@
 # replace:
 # COPY_OUTPUT
 
-echo -e "$NAUTILUS_SCRIPT_SELECTED_FILE_PATHS"\
-    | xargs -i basename "{}"
+echo -e "$NAUTILUS_SCRIPT_SELECTED_FILE_PATHS" \
+    | xargs -i basename "{}" \
     | xargs -i rm "COPY_OUTPUT/{}"
 
-echo -e "$NAUTILUS_SCRIPT_SELECTED_FILE_PATHS"\
-    | xargs -i cp "{}" "COPY_OUTPUT"
+echo -e "$NAUTILUS_SCRIPT_SELECTED_FILE_PATHS" \
+    | xargs -i cp "{}" "COPY_OUTPUT/"
